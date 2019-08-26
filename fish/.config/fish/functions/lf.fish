ce91cc86472b701302f96e4aea7beaaef4dbe285
Fern Zapata <fern@zapata.cc>
2019-08-26 00:05:32 -0600

    Let LF work on non FreeDesktop systems

--- fish/.config/fish/functions/lf.fish
+++ fish/.config/fish/functions/lf.fish
@@ -1,11 +1,12 @@
 if command -qs lf
   function lf -w lf
-    set -l tmp (mktemp -up $XDG_RUNTIME_DIR/lf cd.XXXXXXXX)
+    set -l lfdir $XDG_RUNTIME_DIR/lf /tmp/$USER/lf
+    set -l tmp (mktemp -up $lfdir[1] cd.XXXXXXXX)
     command lf -last-dir-path=$tmp $argv
     if test -f $tmp
-      set -l dir (cat $tmp)
+      read -l dir < $tmp
       rm $tmp
-      test -d $dir && test $dir != $PWD && cd $dir
+      test -d $dir -a $dir != $PWD && cd $dir
     end
   end
 end
