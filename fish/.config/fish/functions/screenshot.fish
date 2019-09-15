e2067448031ad7282846d86c9495cd56b2d2d2b6
Fern Zapata <fern@zapata.cc>
2019-09-14 19:37:21 -0600

    Make the screenshot function less bad

--- fish/.config/fish/functions/screenshot.fish
+++ fish/.config/fish/functions/screenshot.fish
@@ -1,11 +1,12 @@
 function screenshot -a outname -d 'Capture image from screen'
   set -- outname $outname (date "+$HOME/Pictures/Screenshots/%F_%T.png")
+  set -- outname $outname[1]
   if test (uname) = Darwin
-    screencapture -i $outname[1]
+    screencapture -i $outname
   else if test $XDG_SESSION_TYPE = wayland
-    slurp | grim -g - -- $outname[1] 2> /dev/null
+    slurp | grim -g - -- $outname 2>/dev/null
   else
-    maim -s $outname[1]
+    maim -s $outname
   end
-  test -f $outname && echo $outname
+  test -f $outname && realpath $outname
 end
