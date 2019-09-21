78b4178ff9d930ce065c9b44ee1d94595be3cee4
Fern Zapata <fern@zapata.cc>
2019-09-21 16:08:11 -0600

    Let the prompt work on the VT

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
   set_color -o red
-  printf '\n❯❯❯ '
+  set -q DISPLAY && printf '\n❯❯❯ ' || printf '\n>>> '
   set_color normal
 end
--- fish/.config/fish/functions/fish_right_prompt.fish
+++ fish/.config/fish/functions/fish_right_prompt.fish
@@ -1,5 +1,6 @@
 function fish_right_prompt
   set_color -o red
-  printf '❮❮❮ %s' (basename (string replace -r \^$HOME \~ $PWD))
+  set dir (basename (string replace -r \^$HOME \~ $PWD))
+  set -q DISPLAY && printf '❮❮❮ %s' $dir || printf '<<< %s' $dir
   set_color normal
 end
