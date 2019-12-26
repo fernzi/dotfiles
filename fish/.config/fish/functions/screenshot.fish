b9ee6e65b57dda25060c74b73bd29cee852c722a
Fern Zapata <fern@zapata.cc>
2019-12-25 22:34:23 -0700

    Support Gnome in screenshot function

--- fish/.config/fish/functions/screenshot.fish
+++ fish/.config/fish/functions/screenshot.fish
@@ -3,6 +3,8 @@ function screenshot -a outname -d 'Capture image from screen'
   set -- outname $outname[1]
   if test (uname) = Darwin
     screencapture -i $outname
+  else if test $DESKTOP_SESSION = gnome
+    gnome-screenshot -af $outname
   else if test $XDG_SESSION_TYPE = wayland
     slurp | grim -g - -- $outname 2>/dev/null
   else
