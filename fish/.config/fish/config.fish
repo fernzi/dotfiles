a1e851c41e6e79134aa438cd4ffd709d95f30101
Fern Zapata <fern@zapata.cc>
2020-01-16 15:59:04 -0700

    Make sure Firefox works on Gnome

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -27,7 +27,6 @@ set -x NPM_CONFIG_USERCONFIG ~/.config/npm/npmrc
 set -x CCACHE_CONFIGPATH ~/.config/ccache/config
 set -x CCACHE_DIR ~/.cache/ccache
 
-set -x MOZ_ENABLE_WAYLAND 1
 set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
 set -x XCOMPOSEFILE ~/.config/gtk-3.0/Compose
 
@@ -41,6 +40,10 @@ end
 
 set -x FIRESTORM_USER_DIR ~/.local/share/firestorm
 set -x FIRESTORM_X64_USER_DIR $FIRESTORM_USER_DIR
+
+if test $XDG_SESSION_DESKTOP != gnome
+  set -x MOZ_ENABLE_WAYLAND 1
+end
 
 if test $XDG_SESSION_TYPE != wayland
   set -x GDK_SCALE 2
