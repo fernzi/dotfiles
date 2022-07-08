e912a3e188e432dd4a879ebf930af9e942aa4714
Fern Zapata <fern@zapata.cc>
2022-07-08 06:29:14 -0600

    The `myip` function actually works now

--- fish/.config/fish/functions/myip.fish
+++ fish/.config/fish/functions/myip.fish
@@ -1,10 +1,13 @@
-function myip -a mode -d 'Display external IP address'
-  set -l opt
-  switch $mode
-    case 6
-      set opt -6
-    case 4
-      set opt -4
-  end
-  dig $opt +short @resolver1.opendns.com ANY myip.opendns.com
+# Fern's Dotfiles
+# https://github.com/fernzi/dotfiles
+# Find your publicly visible IP address
+
+function myip -d 'Print public IP address'
+	if command -q drill
+		drill -Q @resolver1.opendns.com myip.opendns.com
+	else if command -q dig
+		dig +short @resolver1.opendns.com myip.opendns.com
+	else
+		curl -s https://icanhazip.com
+	end
 end
