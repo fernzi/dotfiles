1e46276cd009531f1734eee50c1fa7c3dd4b714e
Fern Zapata <fern@zapata.cc>
2024-10-03 18:57:44 -0600

    Feat(Zsh): Add an alias for `npm` and `npx`

--- zsh/.config/zsh/rc.d/80-alias.zsh
+++ zsh/.config/zsh/rc.d/80-alias.zsh
@@ -8,10 +8,17 @@ local cmd
 # I don't really keep up with the flavour of the month
 # AUR helper, so which ones and in which order this checks
 # is kind of arbitrary.
-
 for cmd in yay paru pacman; do
 	if [[ -v commands[${cmd}] ]]; then
 		alias p=${cmd}
 		break
 	fi
 done
+
+# If the cost of not creating untracked files
+# is defining an alias, I'll gladly pay it.
+if [[ -v commands[corepack] ]]; then
+        for c in npm npx pnpm pnpx; do
+                alias ${c}="corepack ${c}"
+        done
+fi
